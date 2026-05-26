06d08627ccb16f963c71f80cc6afeb56858e8be7
lftour <lftour@users.noreply.github.com>
2026-05-26 19:55:03 +0000

    deploy: 7fd6516899b99f816303e83f1299e76d42f80d00

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,3 +1,12 @@
-const firebaseConfig = {apiKey:AIzaSyB4uDwwdNqqnWmfo-grGMXRRf1gXT0Aaes,authDomain:tm-backend-1f6d8.firebaseapp.com,databaseURL:https://tm-backend-1f6d8-default-rtdb.firebaseio.com,projectId:tm-backend-1f6d8,storageBucket:tm-backend-1f6d8.firebasestorage.app,messagingSenderId:561205366355,appId:1:561205366355:web:ef97ec7dd158deb09dc0a6,measurementId:G-10HM8F896V};
+const firebaseConfig = {
+  apiKey: "AIzaSyB4uDwwdNqqnWmfo-grGMXRRf1gXT0Aaes",
+  authDomain: "tm-backend-1f6d8.firebaseapp.com",
+  databaseURL: "https://tm-backend-1f6d8-default-rtdb.firebaseio.com",
+  projectId: "tm-backend-1f6d8",
+  storageBucket: "tm-backend-1f6d8.firebasestorage.app",
+  messagingSenderId: "561205366355",
+  appId: "1:561205366355:web:ef97ec7dd158deb09dc0a6",
+  measurementId: "G-10HM8F896V"
+};
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
